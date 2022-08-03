aab3ff9e801b1eaf78b7498778de5bf276abb464
Tautvydas-coder <b7&*%13kHqvP>
2022-08-03 14:39:54 +0300

    upd:xpath,add:firefox browser

--- Resources/global_variables.py
+++ Resources/global_variables.py
@@ -1,23 +1,25 @@
 # Browser
-BROWSER = "chrome"
-NEW_TAB = "chrome://newtab/"
+BROWSER = "Firefox"
+FFOX_NEW_TAB = "about:newtab"
 
+CHROME_BROWSER = "chrome"
+CHROME_NEW_TAB = "chrome://newtab/"
 # Window
 MAX_WINDOW = "Maximize Browser Window"
 
 # Speed
 SELSPEED = 1.5
-SLEEP_TIME = 2
+SLEEP_TIME = 1
 
 # URL
 GAS_PRICE_URL = "http://www.degalukainos.lt/"
-GMAIL_LOGIN_URL = "https://accounts.google.com/ServiceLogin?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service%27%27=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
+GMAIL_LOGIN_URL = "https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=1209600&osid=1&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&emr=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
 
 # Buttons
 FIND_BUTTON = "xpath://input[@value='Ieškoti']"
 GMAIL_SEND_BUTTON = "xpath://div[@aria-label='Siųsti ‪(Ctrl –Enter)‬']"
 LOGIN_PASSWORD_BUTTON_NEXT = 'xpath://*[@id="passwordNext"]'
-CREATE_LETTER_BUTTON = "xpath:/html[1]/body[1]/div[7]/div[3]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]"
+CREATE_LETTER_BUTTON = "xpath:/html/body/div[7]/div[3]/div/div[2]/div[1]/div[1]/div/div"
 LOGIN_NAME_BUTTON_NEXT = "xpath://div[@id='identifierNext']/div/button/span"
 
 # City
@@ -29,7 +31,7 @@ EMAIL_NAME = "work.selenium@gmail.com"
 PASSWORD = "Selenium1]"
 
 # Name and pass login pages box
-BOX_LOGIN_NAME = "id=identifierId"
+BOX_LOGIN_NAME = 'xpath://*[@id="identifierId"]'
 BOX_LOGIN_PASSWORD = 'xpath://*[@id="password"]/div[1]/div/div[1]/input'
 
 # Move mouse
